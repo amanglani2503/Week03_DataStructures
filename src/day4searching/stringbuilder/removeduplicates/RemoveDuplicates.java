5e436a83a4ad0bc158824fe962b588ca441e06a1
amanglani2503 <ambermanglani8871@gmail.com>
2025-02-04 11:04:40 +0530

    [Amber] Add. Function that removes duplicates characters from a string

---
+++ src/day4searching/stringbuilder/removeduplicates/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+package day4searching.stringbuilder.removeduplicates;
+
+import java.util.HashSet;
+
+public class RemoveDuplicates {
+    public static String removeDuplicates(String string){
+        // creating string builder object for given string
+        StringBuilder buildString = new StringBuilder(string);
+
+        // string to store string without duplicates
+        StringBuilder removedDuplicates = new StringBuilder();
+
+        // to store unique characters
+        HashSet<Character> seen = new HashSet<>();
+
+        for(char character : string.toCharArray()){
+            if(!seen.contains(character)){
+                seen.add(character);
+                removedDuplicates.append(character);
+            }
+        }
+
+        return removedDuplicates.toString();
+    }
+
+    // main method
+    public static void main(String[] args) {
+        // input string
+        String string = "Good Morning";
+
+        // storing string containing unique characters
+        String removedDuplicates = removeDuplicates(string);
+
+        System.out.println("Original String : " + string);
+        System.out.println("String containing unique charaters : " + removedDuplicates);
+    }
+}
