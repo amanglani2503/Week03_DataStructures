694f4632145974224af2f0bc3ad459997165616b
amanglani2503 <ambermanglani8871@gmail.com>
2025-02-04 11:03:49 +0530

    [Amber] Add. Function that reverses a string using STringBuilder

---
+++ src/day4searching/stringbuilder/stringreversal/ReverseStringUsingStringBuffer.java
@@ -0,0 +1,27 @@
+package day4searching.stringbuilder.stringreversal;
+
+public class ReverseStringUsingStringBuffer {
+    // method to reverse a string using StringBuffer
+    public static String reverseString(String string){
+        // creating StringBuilder object of given string
+        StringBuilder builderString = new StringBuilder(string);
+
+        // reversing the builder string
+        builderString.reverse();
+
+        // returning the reversed String
+        return builderString.toString();
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // String to be reversed
+        String string = "Hello Everyone";
+
+        // storing reversed string
+        String reversedString = reverseString(string);
+
+        System.out.println("Original String : " + string);
+        System.out.println("Reversed String : " + reversedString);
+    }
+}
